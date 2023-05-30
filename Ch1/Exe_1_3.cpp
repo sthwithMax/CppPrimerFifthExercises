1e29450d1b55fee2c6e53ff5c6ff286f5ff072b1
MaxLiu <max.liu@philips.com>
2023-05-30 14:27:44 +0800

    Exercises Section 1.3

---
+++ Ch1/Exe_1_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"\nExercise 1.7: Compile a program that has incorrectly nested comments.";
+    cout<<"\n/s/";
+
+    cout<<"\n\nExercise 1.8: Indicate which, if any, of the following output statements are legal:";
+    cout<<"\n The 1st and the 2nd are legal, the others are not.";
+    return 0;
+}
