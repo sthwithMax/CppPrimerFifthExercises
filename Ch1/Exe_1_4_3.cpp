d5638a9df1a3ca84768e4bbfd50b2f382b619e34
MaxLiu <max.liu@philips.com>
2023-05-30 15:37:04 +0800

    Exercises Section 1.4.3

---
+++ Ch1/Exe_1_4_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"\nExercise 1.16: Write your own version of a program that prints the sum of a set of integers read from cin.";
+    int v1=0, v2=0;
+    cout<<"\nPlease input v1:";
+    cin >> v1;
+    cout<<"Please input v2:";
+    cin >> v2;
+    cout << "The sum of v1 and v2 is " << v1+v2;
+    return 0;
+}
